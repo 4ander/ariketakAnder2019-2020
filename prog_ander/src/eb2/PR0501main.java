6c38e5509e220d2df16c46336d0f651af6473743
4ander <go.za.an@gmail.com>
2019-12-11 11:02:02 +0100

    asfasfasfasf

--- prog_ander/src/eb2/PR0501main.java
+++ prog_ander/src/eb2/PR0501main.java
@@ -6,65 +6,106 @@ import java.util.ArrayList;
 
 
 public class PR0501main {
+	static ArrayList<PR0501ONDO>kont=new ArrayList();
+	static Scanner teklatua=new Scanner(System.in);
+	static String iz;
+	static boolean izenakerr=false;
+	static int i;
+
+
+
 	public static void main(String[] args) {
 		Scanner teklatua=new Scanner(System.in);
-		ArrayList<PR0501ONDO>kont=new ArrayList();
 		int zenbatobj=0;
-		String aukera;
-		System.out.println("objetu berri bat?");
-		aukera=teklatua.next();
+		String aukera="bai";
 		
 		
-		for(int i=0;!aukera.equals("ez");i++) {
-			PR0501ONDO z1=new PR0501ONDO();
-			kont.add(i,z1);
-			System.out.println("esan objetuaren izena: ");
-			kont.get(i).izena=teklatua.next();
-			System.out.println("esan zenbat elementu dauden");
-			kont.get(i).kopurua=teklatua.nextInt();
-			kont.get(i).zenbat=z1.zenbat+1;
-			System.out.println("objetu berri bat?");
-			aukera=teklatua.next();
-		}
 		
 		
-		System.out.println(kont.get(0).zenbat+" objetu sortu dira ");
-		for(int w=0;w<kont.size();w++) {
-			System.out.println(kont.get(w).kopurua+" "+kont.get(w).izena+" daude");
-		}
-		System.out.println("objeturen bat modifikatu? ");
-		aukera=teklatua.next();
+	
 		while(!aukera.equals("ez")) {
 			System.out.println("1, gehitzeko");
 			System.out.println("2, kentzeko");
 			System.out.println("3, aldaketa zuzenean egiteko");
+			System.out.println("4, dena ikusteko");
 			String aukeratu=teklatua.next();
 			
 			
 			switch(aukeratu) {
-				1:
-					
+			case "1":
+					gehitu();
+					break;
+			case "2":
+					kendu();
+					break;
+			case "3":
+					aldatu();
+					break;
+			case "4":
+				erakutsi();
+				aukera="ez";
+				break;
 			}
+			
 		}
 		
 		
 		
 	}
-	public void gehitu() {
+	public static void gehitu() {
+		System.out.println("esan izena: ");
+		String ize=teklatua.next();
+		String ize1=ize;
+		iz=ize1;
+		System.out.println("esan kopurua: ");
+		int kop=teklatua.nextInt();
+		bilatu();
+		
+		
+		if(izenakerr==true) {
+			System.out.println("izena errepikatu duzu, ez da objetua sortuko");
+		}
+		else {
+			PR0501ONDO p1=new PR0501ONDO();
+			p1.izena=ize;
+			p1.kopurua=kop;
+			p1.zenbat=p1.zenbat+1;
+			kont.add(p1);
+		}
+	}
+	public static void kendu() {
+		System.out.println("Esan ezabatu nahi duzun objetuaren izena");
+		iz=teklatua.next();
+		bilatu();
+		kont.remove(i);
+		kont.get(0).zenbat=kont.get(0).zenbat-1;
+	}
+	public static void aldatu() {
+		System.out.println("zein aldatu nahi duzu? ");
+		iz=teklatua.next();
+		bilatu();
+		if(izenakerr==true) {
+			System.out.println("esan izen berria: ");
+			String izenberria=teklatua.next();
+			System.out.println("kantitate berria: ");
+			int kantberria=teklatua.nextInt();
+			kont.get(i).izena=izenberria;
+			kont.get(i).kopurua=kantberria;
+		}
 		
 	}
-	public void kendu() {
-		
+	public static void bilatu() {
+		for(i=0;i<kont.size();i++) {
+			if(kont.get(i).izena.equals(iz)) {
+				izenakerr=true;
+				break;
+			}
+		}
 	}
-	public void aldatu() {
-		
-	}
-	public void bilatu() {
-		Scanner teklatua=new Scanner(System.in);
-		System.out.println("zein objetu?: ");
-		String aurkitzeko=teklatua.next();
-		
-		for(static int x=0;x<)
+	public static void erakutsi() {
+		for(int i=0;i<kont.size();i++) {
+			System.out.println(kont.get(i).izena+" objetuak "+kont.get(i).kopurua+" elementu ditu");
+		}
 	}
 
 }
